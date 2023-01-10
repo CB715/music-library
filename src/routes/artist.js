f9d9f876519b14a0bbed3310b7923284c538825b
Carl Beckley <carlbeckley@gmail.com>
2023-01-10 16:25:38 +0000

    added route and controller function to delete artists by ID

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -7,7 +7,8 @@ const router = express.Router();
 router.post('/', artistController.createArtist);
 router.get('/', artistController.getAllArtists);
 router.get('/:id', artistController.getArtistById);
-router.put('/:id', artistController.updateArtistsPut)
-router.patch('/:id', artistController.updateArtistPatch)
+router.put('/:id', artistController.updateArtistsPut);
+router.patch('/:id', artistController.updateArtistPatch);
+router.delete('/:id', artistController.deleteArtist);
 
 module.exports = router;
